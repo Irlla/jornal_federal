fd3996f7ec2b738d136223f9f49508629e3719be
Irlla <irllalopes07@gmail.com>
2019-12-06 12:07:26 -0300

    pastas

---
+++ jornal_federal_project/urls.py
@@ -0,0 +1,25 @@
+"""jornal_federal_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from paginas.views import HomePageView, NoticiaPageView, MatriculaPageView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', MatriculaPageView.as_view() , name ='matricula'),
+    path('noticia/', NoticiaPageView.as_view() , name ='noticia'),
+    path('home/', HomePageView.as_view() , name ='home'),
+]
